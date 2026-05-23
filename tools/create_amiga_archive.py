71473685a8c7856c8401c8519276cd97a38d4183
Jean-François Fabre <jotd666@gmail.com>
2026-05-23 08:39:48 +0200

    optimized labels, added keyboard controls

--- tools/create_amiga_archive.py
+++ tools/create_amiga_archive.py
@@ -1,12 +1,12 @@
-import subprocess,os,glob,shutil
+import subprocess,os,glob,shutil,pathlib
 
-progdir = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
+progdir = pathlib.Path(os.path.join(os.path.dirname(__file__),os.pardir)).absolute()
 
 gamename = "xevious"
 # JOTD path for cranker, adapt to wh :)
 os.environ["PATH"] += os.pathsep+r"K:\progs\cli"
 
-cmd_prefix = ["make","-f",os.path.join(progdir,"makefile.am")]
+cmd_prefix = ["make","-f",progdir/"makefile.am"]
 
 subprocess.check_call(cmd_prefix+["clean"],cwd=os.path.join(progdir,"src"))
 
@@ -16,12 +16,12 @@ for s in ["convert_sounds.py","convert_graphics.py"]:
 subprocess.check_call(cmd_prefix+["RELEASE_BUILD=1"],cwd=os.path.join(progdir,"src"))
 # create archive
 
-outdir = os.path.join(progdir,f"{gamename}_HD")
+outdir = progdir / "dist" / f"{gamename}_HD"
 if os.path.exists(outdir):
-    for x in glob.glob(os.path.join(outdir,"*")):
-        os.remove(x)
-else:
-    os.mkdir(outdir)
+    shutil.rmtree(outdir)
+
+outdir.mkdir(exist_ok=True,parents=True)
+
 for file in ["readme.md","instructions.txt",gamename,f"{gamename}.slave"]:
     shutil.copy(os.path.join(progdir,file),outdir)
 
@@ -30,3 +30,10 @@ shutil.copy(os.path.join(progdir,"assets","amiga","Xevious.info"),outdir)
 
 # pack the file for floppy
 subprocess.check_output(["cranker_windows.exe","-f",os.path.join(progdir,gamename),"-o",os.path.join(progdir,f"{gamename}.rnc")])
+
+
+arcname = progdir / f"Xevious1200_HD.lha"
+arcname.unlink(missing_ok=True)
+cmd = ["lha","-r","a",arcname,"*"]
+
+subprocess.run(cmd,cwd=outdir.parent,check=True)
